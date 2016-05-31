5409d85f1ab73cde17164bbe03060cbba79b8ff1
Aidan Christopher Haughey <nazghoul22@gmail.com>
2016-05-31 12:40:37 -0700

    mail server bizz

--- public/mail/contact.js
+++ public/mail/contact.js
@@ -1,23 +1,8 @@
-var express = require('express');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
-var app = express();
 
-var port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
-app.listen(port, function()  {
-    console.log('Our app is running on http://localhost' + port);
-});
-
-app.post('/endpoint', function(req, res) {
+app.post('././mail/contact.js', function(req, res) {
     var mailOpts, smtpTrans;
     
     
